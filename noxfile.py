21781d8ff2cdc9ed6302998715635b8880dbda85
David Kasakaitis <davidkasakaitis@proton.me>
2025-04-05 11:14:40 +0100

    test(nox): correctly test all python versions locally

--- noxfile.py
+++ noxfile.py
@@ -3,13 +3,13 @@ import nox
 nox.options.default_venv_backend = "uv|virtualenv"
 
 
-@nox.session(python=["3.12"])
+@nox.session(python=["3.10"])
 def lint(session: nox.Session):
     session.run("uv", "sync", "--no-dev", "--group", "lint")
     session.run("uv", "run", "ruff", "check", "src")
 
 
-@nox.session(python=["3.12"])
+@nox.session(python=["3.10"])
 def type_check(session: nox.Session):
     session.run("uv", "sync", "--no-dev", "--group", "type")
     session.run("uv", "run", "mypy", "src")
@@ -17,7 +17,15 @@ def type_check(session: nox.Session):
 
 @nox.session(python=["3.10", "3.11", "3.12", "3.13"])
 def test(session: nox.Session):
-    session.run("uv", "sync", "--no-dev", "--group", "test")
+    py_version = f"--python={session.python}"
+    session.run("uv", "sync", py_version, "--no-dev", "--group", "test")
     session.run(
-        "uv", "run", "pytest", "--cov-branch", "--cov-report=xml", "-n", "auto"
+        "uv",
+        "run",
+        py_version,
+        "pytest",
+        "--cov-branch",
+        "--cov-report=xml",
+        "-n",
+        "auto",
     )
