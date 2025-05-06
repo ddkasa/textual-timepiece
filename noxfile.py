e536ea9f2340cba7c852a8dfff87882068a3dc3d
David Kasakaitis <davidkasakaitis@proton.me>
2025-05-06 10:06:39 +0100

    ci(nox): use `--active` flag

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import nox
 
 nox.options.default_venv_backend = "uv|virtualenv"
@@ -5,24 +7,33 @@ nox.options.default_venv_backend = "uv|virtualenv"
 
 @nox.session(python=["3.10"])
 def lint(session: nox.Session) -> None:
-    session.run("uv", "sync", "--no-dev", "--group", "lint")
-    session.run("uv", "run", "ruff", "check", "src")
+    session.run("uv", "sync", "--active", "--no-dev", "--group", "lint")
+    session.run("uv", "run", "--active", "ruff", "check", "src")
 
 
 @nox.session(python=["3.10"])
 def type_check(session: nox.Session) -> None:
-    session.run("uv", "sync", "--no-dev", "--group", "type")
-    session.run("uv", "run", "mypy", "src")
+    session.run("uv", "sync", "--active", "--no-dev", "--group", "type")
+    session.run("uv", "run", "--active", "mypy", "src")
 
 
 @nox.session(python=["3.10", "3.11", "3.12", "3.13"])
 def test(session: nox.Session) -> None:
-    py_version = f"--python={session.python}"
-    session.run("uv", "sync", py_version, "--no-dev", "--group", "test")
+    python_version = f"--python={session.python}"
+    session.run(
+        "uv",
+        "sync",
+        "--active",
+        python_version,
+        "--no-dev",
+        "--group",
+        "test",
+    )
     session.run(
         "uv",
         "run",
-        py_version,
+        "--active",
+        python_version,
         "pytest",
         "--cov-branch",
         "--cov-report=xml",
