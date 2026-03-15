11f82c7f5ce2f458c6079d1824289e7044e6654f
David Kasakaitis <davidkasakaitis@proton.me>
2026-03-15 10:11:59 +0000

    style(nvim): run formatter

--- .nvim.lua
+++ .nvim.lua
@@ -1,22 +1,23 @@
-vim.lsp.enable("ruff")
-vim.lsp.enable("pyright")
+vim.lsp.enable({ "ruff", "pyright" })
 
 local cwd = vim.fn.getcwd()
 
+require("dap-python").setup("debugpy-adapter")
+
 table.insert(require("dap").configurations.python, {
-    type = "python",
-    request = "launch",
-    name = "Debug File",
-    program = "${file}",
+	type = "python",
+	request = "launch",
+	name = "Debug File",
+	program = "${file}",
 })
 table.insert(require("dap").configurations.python, {
-    type = "python",
-    request = "launch",
-    justMyCode = false,
-    name = "Launch Application",
-    redirectOutput = false,
-    program = cwd .. "/src/textual_timepiece/__main__.py",
-    cwd = cwd,
-    python = cwd .. "/.venv/bin/python",
-    console = "externalTerminal",
+	type = "python",
+	request = "launch",
+	justMyCode = false,
+	name = "Launch Application",
+	redirectOutput = false,
+	program = cwd .. "/src/textual_timepiece/__main__.py",
+	cwd = cwd,
+	python = cwd .. "/.venv/bin/python",
+	console = "externalTerminal",
 })
