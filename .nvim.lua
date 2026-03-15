4a3fbff79f13fccdbab38e992fff16a141a81375
David Kasakaitis <davidkasakaitis@proton.me>
2026-03-15 10:20:15 +0000

    chore(nvim): refactor config (#9)

--- .nvim.lua
+++ .nvim.lua
@@ -1,21 +1,23 @@
-local enable_lsp = { "ruff", "mypy" }
+vim.lsp.enable({ "ruff", "pyright" })
 
 local cwd = vim.fn.getcwd()
 
+require("dap-python").setup("debugpy-adapter")
+
 table.insert(require("dap").configurations.python, {
-    type = "python",
-    request = "launch",
-    name = "Debug File",
-    program = "${file}",
+	type = "python",
+	request = "launch",
+	name = "Debug File",
+	program = "${file}",
 })
 table.insert(require("dap").configurations.python, {
-    type = "python",
-    request = "launch",
-    justMyCode = false,
-    name = "Launch Application",
-    redirectOutput = false,
-    program = cwd .. "/src/textual_timepiece/__main__.py",
-    cwd = cwd,
-    python = cwd .. "/.venv/bin/python",
-    console = "externalTerminal",
+	type = "python",
+	request = "launch",
+	justMyCode = false,
+	name = "Launch Application",
+	redirectOutput = false,
+	program = cwd .. "/src/textual_timepiece/__main__.py",
+	cwd = cwd,
+	python = cwd .. "/.venv/bin/python",
+	console = "externalTerminal",
 })
